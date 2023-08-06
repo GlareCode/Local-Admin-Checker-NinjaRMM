71123db48354cd670d4862e3a515056034791b62
GlareCode <glarecode@outlook.com>
2023-08-06 12:52:57 -0400

    Update localAdminChecker.ps1
    Renamed functions, added more checks, added pause times, and included user input options.

--- localAdminChecker.ps1
+++ localAdminChecker.ps1
@@ -13,7 +13,7 @@
     .OUTPUTS
 
         Has four outputs.
-        1: '$gen' calls function 'generate-password' and '$gen' will not change unless function 'creaete-localAdmin' is called again.
+        1: '$gen' calls function 'Initialize-Password' and '$gen' will not change unless function 'creaete-localAdmin' is called again.
         2: function 'create-localAdmin' will print if '$who' is not a user.
         3: function 'check-localAdmins' will print if '$who' is or is not a localadmin.
 
@@ -27,83 +27,118 @@
 
         PS> find-localAdmins
 
-    .LINK
-        
 #>
 
+$gotchya = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
 
-function generate-Password{
-    Add-Type -AssemblyName system.web
-    [System.Web.Security.Membership]::GeneratePassword(12, 1)
+if ($gotchya -eq $False){
+
+    Write-Host "`n**************Please reopen PowerShell as Administrator***************"
+    Write-Host "**************Please reopen PowerShell as Administrator***************"
+    Write-Host "**************Please reopen PowerShell as Administrator***************`n"
+    
+    $exit = Read-Host "Press 0 to Exit"
+
+    if ($exit -ne 0){
+        stop-process -id $PID
+    }Else{
+        stop-process -id $PID
+    }
+
+}
+
+function Initialize-Restart {
+
+    $restart = Read-Host "Start over? `nPress 1 for Yes and 2 for No`n"
+
+    if ($restart -eq "1"){
+        Measure-LocalAdmins
+    }ElseIf ($restart -eq "2"){
+        stop-process -id $PID
+    }Else{
+        Initialize-Restart
+    }
+
 }
 
 
-function create-localAdmin {
+function Initialize-Password {                             # Generate random password
 
-    # If '$g' is still '$false' then the user is not created at all and will create the user with '$who and $pass', then toggles '$g' to '$True'
+    Add-Type -AssemblyName system.web
+    [System.Web.Security.Membership]::GeneratePassword(12, 1)
+
+    $output = Get-LocalGroupMember -Name "Administrators" | Select-Object -ExpandProperty Name
+    $output
+    write-host "$who generated password is $gen"
+    
+    Initialize-Restart
+
+}
+
+
+function Grant-LocalAdmin {
+
+    # If '$g' is still '$false' then the user does not exist-
+    # will create the user with '$who and $pass'-
+    # then toggles '$g' to '$True'
+
     Write-Host "$who is not a user, creating user now..."
+    Start-Sleep -Seconds 4
 
-    $gen = generate-Password
+    $gen = Initialize-Password
 
     & net user /add $who $gen
     & net localgroup administrators $who /add
     
     # When the localAdmin is created, it will place the generated password in Ninja Custom Fields.
-    #Ninja-Property-Set Administrator-Password $gen
+    # Ninja-Property-Set Administrator-Password $gen
+
 }
 
 
-function check-localAdmins {
-         
-    # If '$who' is a localAdministrator, '$localAdministrators' will be greater than 0.
+function Find-LocalAdmins {                                # If '$who' is a localAdministrator, '$localAdministrators' will be greater than 0.
+    
     $localAdministrators = Get-LocalGroupMember -Name "Administrators" | Where-Object {$_.Name -eq "$env:COMPUTERNAME\$who"} | Select-Object -ExpandProperty Name
 
     if ($localAdministrators -gt 0){
         Write-Host "$who is a local administrator"
     }else{
         Write-Host "$who is not a local administrator"
+        Grant-LocalAdmin
     }
+
 }
 
 
-function find-localAdmins {
-
-    # Finds all local users and places into variable localUsers.
+function Measure-LocalAdmins {                             # Finds all local users and places into variable localUsers.
+    
     Get-LocalUser | Where-Object {$_.Enabled -eq $true} | select-object -ExpandProperty Name -OutVariable localUsers | Out-Null
+    
+    $g = "$False"                                          # '$g' will toggle to '$True' if '$who' user is already in the system.
+    $who = Read-Host "`nWhich user are you searching for?`n"
 
-    # '$g' will toggle to '$True' if '$who' user is already in the system.
-    $g = "$False"
-    $who = "testuser"
-
-    for ($i = 0; $i -le $localUsers.count; $i++){
-
+    for ($i = 0; $i -le $localUsers.count; $i++) {
         if ($localUsers[$i] -eq $who){
             $g = "$True"
         }
     }
-    
-    if ($pass.Length -gt 14){
+
+    if ($pass.Length -gt 14) {
         Write-Host "$pass is longer than 14 characters and will not work with dos"
-        break
+        Start-Sleep -Seconds 3
+        Measure-LocalAdmins
     }
 
-    # If '$g' is '$False' it will call function 'create-localAdmin'.
-    if ($g -eq "$false"){
-        create-localAdmin
+    if ($g -eq "$false") {                                  # If '$g' is '$False' it will call function 'Find-LocalAdmins'.
+        Find-LocalAdmins
         $g = "$True"
+    }Elseif ($g -eq "$True") {                              # If '$g' is '$True' it will call function 'Find-LocalAdmins'.
+        Find-LocalAdmins
     }
 
-    # If '$g' is '$True' it will call function 'check-localAdmins'.
-    if ($g -eq "$True"){
-        check-localAdmins
-    }
 }
 
 
-find-localAdmins
+Measure-LocalAdmins
 
-$output = Get-LocalGroupMember -Name "Administrators" | Select-Object -ExpandProperty Name
-$output
-write-host "$who generated password is $gen"
-
-#Ninja-Property-Set Administrator $output
+# Ninja-Property-Set Administrator $output
