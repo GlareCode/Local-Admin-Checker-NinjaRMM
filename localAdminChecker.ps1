d197613962a7d1b763d368c0ee6b44e2a837b303
GlareCode <glarecode@outlook.com>
2023-08-09 13:22:47 -0400

    Update localAdminChecker.ps1
    - reafctored
    - added NinjaRMM functionality
    - added error handling
    - added outputs for diagnosing

--- localAdminChecker.ps1
+++ localAdminChecker.ps1
@@ -1,144 +1,122 @@
 ﻿<#
     .SYNOPSIS
 
-        finds a user.
+        finds a user in NinjaRMM parameter
         if not a user; creates user.
-        if user exist; checks if localadmin.
+        if user exists; checks if localadmin.
+        if or if not localadmin, resets password with generated.
         if not localadmin; turns into localadmin.
 
     .INPUTS
 
-        None. but alter the '$who' variable in function 'find-localAdmins' to reflect the user you're interested in.
+        Requires NinjaRMM parameter ( Type in a username in param before running script )
 
     .OUTPUTS
 
-        Has four outputs.
-        1: '$gen' calls function 'Initialize-Password' and '$gen' will not change unless function 'creaete-localAdmin' is called again.
-        2: function 'create-localAdmin' will print if '$who' is not a user.
-        3: function 'check-localAdmins' will print if '$who' is or is not a localadmin.
+        "Setting NinjaRMM Custom Attribute (Administrators)"
+        "Setting NinjaRMM Custom Attribute (GeneratePassword)"
+        "$User Exists"
+        "$User does not exist"
+        "Trying to make $User Admin..."
+        "An error occured in Find-LocalAdmin...Exiting - 00"
+        "Something unexpected happend in Find-LocalAdmin...Exiting - 00"
+        "Something unexpected happend in Find-LocalAdmin...Exiting - 01"
+        "An error occured in Find-LocalAdmin...Exiting - 01"
 
     .EXAMPLE
-
-        $who is not a user, creating user now...
-        $who is already a local administrator
-        $who is not a local administrator
-
-    .EXAMPLE
-
-        PS> find-localAdmins
+        PS> Find-LocalAdmin
 
 #>
+
+
+param(
+  [String]$variable1='')
+
+if ($variable1 -eq $null){
+    return "Need NinjaRMM parameter set...Exiting"
+}Else{
+    Write-Host "NinjaRMM parameter set to $variable1...Continuing"
+}
 
 $gotchya = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
 
 if ($gotchya -eq $False){
-
-    Write-Host "`n**************Please reopen PowerShell as Administrator***************"
     Write-Host "**************Please reopen PowerShell as Administrator***************"
-    Write-Host "**************Please reopen PowerShell as Administrator***************`n"
-    
-    $exit = Read-Host "Press 0 to Exit"
-
-    if ($exit -ne 0){
-        stop-process -id $PID
-    }Else{
-        stop-process -id $PID
-    }
-
+    Write-Host "**************Please reopen PowerShell as Administrator***************"
+    Write-Host "**************Please reopen PowerShell as Administrator***************"
+    stop-process -id $PID
 }
 
-function Initialize-Restart {
+function Find-LocalAdmin {
+    #$User = "test"
+    $UserFind = Get-LocalUser | Where-Object Name -like "$variable1"
+    $UserFind
 
-    $restart = Read-Host "Start over? `nPress 1 for Yes and 2 for No`n"
+    if ($UserFind.Name -like "$variable1"){
+        Write-Host "$variable1 Exists"
 
-    if ($restart -eq "1"){
-        Measure-LocalAdmins
-    }ElseIf ($restart -eq "2"){
-        stop-process -id $PID
-    }Else{
-        Initialize-Restart
-    }
+        if ($UserFind.Enabled -eq "True"){
+            Write-Host "$variable1 is Admin"
+            
+            try {
+                # Generate and assign password
+                Add-Type -AssemblyName system.web
+                $pass = [System.Web.Security.Membership]::GeneratePassword(12, 1)
+                Write-Host "Setting password for $variable1"
+                & net user $variable1 $pass
+                Write-Host "The password is $pass"
+            }
+            catch {
+                return "An error occured in Find-LocalAdmin...Exiting - 03"
+            }
 
-}
+            try {
+                # Set Ninja Custom Attributes
+                Write-Host "Setting NinjaRMM Custom Attribute (GeneratePassword)"
+                Ninja-Property-Set generatepassword $pass
+                Write-Host "Setting NinjaRMM Custom Attribute (Administrators)"
+                Ninja-Property-Set administrators $variable1
+            }
+            catch {
+                return "An error occured in Find-LocalAdmin...Exiting - 02"
+            }
 
+            return "`nJobs done`n"
 
-function Initialize-Password {                             # Generate random password
-
-    Add-Type -AssemblyName system.web
-    [System.Web.Security.Membership]::GeneratePassword(12, 1)
-
-    $output = Get-LocalGroupMember -Name "Administrators" | Select-Object -ExpandProperty Name
-    $output
-    write-host "$who generated password is $gen"
-    
-    Initialize-Restart
-
-}
-
-
-function Grant-LocalAdmin {
-
-    # If '$g' is still '$false' then the user does not exist-
-    # will create the user with '$who and $pass'-
-    # then toggles '$g' to '$True'
-
-    Write-Host "$who is not a user, creating user now..."
-    Start-Sleep -Seconds 4
-
-    $gen = Initialize-Password
-
-    & net user /add $who $gen
-    & net localgroup administrators $who /add
-    
-    # When the localAdmin is created, it will place the generated password in Ninja Custom Fields.
-    # Ninja-Property-Set Administrator-Password $gen
-
-}
-
-
-function Find-LocalAdmins {                                # If '$who' is a localAdministrator, '$localAdministrators' will be greater than 0.
-    
-    $localAdministrators = Get-LocalGroupMember -Name "Administrators" | Where-Object {$_.Name -eq "$env:COMPUTERNAME\$who"} | Select-Object -ExpandProperty Name
-
-    if ($localAdministrators -gt 0){
-        Write-Host "$who is a local administrator"
-    }else{
-        Write-Host "$who is not a local administrator"
-        Grant-LocalAdmin
-    }
-
-}
-
-
-function Measure-LocalAdmins {                             # Finds all local users and places into variable localUsers.
-    
-    Get-LocalUser | Where-Object {$_.Enabled -eq $true} | select-object -ExpandProperty Name -OutVariable localUsers | Out-Null
-    
-    $g = "$False"                                          # '$g' will toggle to '$True' if '$who' user is already in the system.
-    $who = Read-Host "`nWhich user are you searching for?`n"
-
-    for ($i = 0; $i -le $localUsers.count; $i++) {
-        if ($localUsers[$i] -eq $who){
-            $g = "$True"
+        }Else{
+            Write-Host "$variable1 is not Admin"
+            
+            try {
+                $ErrorActionPreference = "Stop"
+                Write-Host "Trying to make $variable1 Admin..."
+                & net localgroup Administrators $variable1 /add
+            }
+            catch {
+                return "An error occured in Find-LocalAdmin...Exiting - 01"
+            }
+            return "Something unexpected happend in Find-LocalAdmin...Exiting - 01"
         }
-    }
+    }Else{
+        Write-Host "$variable1 does not exist"
+        
+        try {
+            $ErrorActionPreference = "Stop"
 
-    if ($pass.Length -gt 14) {
-        Write-Host "$pass is longer than 14 characters and will not work with dos"
-        Start-Sleep -Seconds 3
-        Measure-LocalAdmins
-    }
+            Write-Host "Creating $variable1"
+            & net user $variable1 teMp*3dG4gd*2k /add
 
-    if ($g -eq "$false") {                                  # If '$g' is '$False' it will call function 'Find-LocalAdmins'.
-        Find-LocalAdmins
-        $g = "$True"
-    }Elseif ($g -eq "$True") {                              # If '$g' is '$True' it will call function 'Find-LocalAdmins'.
-        Find-LocalAdmins
+            Write-Host "Adding $variable1 to Admin Group"
+            & net localgroup Administrators $variable1 /add
+
+        }
+        catch {
+            return "An error occured in Find-LocalAdmin...Exiting - 00"
+        }
+        return Find-LocalAdmin
     }
+    return "Something unexpected happend in Find-LocalAdmin...Exiting - 00"
 
 }
 
+Find-LocalAdmin
 
-Measure-LocalAdmins
-
-# Ninja-Property-Set Administrator $output
